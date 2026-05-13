f22bbceda630f5df830925dff61563a78da9e389
fnamg14 <githupsignals@gmail.com>
2026-05-13 11:37:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,9 @@ app.get('/', (req, res) => {
 app.use('/api/signals', signalsRoutes);
 
 const signalAnalyzer = require('./services/signalAnalyzer');
-signalAnalyzer.startBackgroundRefresh();
+setTimeout(() => {
+  signalAnalyzer.startBackgroundRefresh();
+}, 180000);
 
 app.use((req, res) => {
   res.status(404).json({ 
